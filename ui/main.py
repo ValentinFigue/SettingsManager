473ba24ecb20bb06fd0c9b16fcf4bad9eb74c7bc
ValentinFigue <valentin.figue@orange.fr>
2023-05-31 18:19:57 +0200

    Added Pyside6 require and main UI file

---
+++ ui/main.py
@@ -0,0 +1,14 @@
+import sys
+
+from PySide6 import QtCore, QtGui, QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
